13e146eac87896c9e7dfb3995e3360758941d7b3
partelsandra <sandra.partel.001@voco.ee>
2024-01-16 20:34:21 +0200

    commit update

--- public/index.php
+++ public/index.php
@@ -21,21 +21,14 @@ AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
-$app->get('/', 'App\Controller\AlbumsController:default');
-$app->get('/details/{id:[0-9]+}', 'App\Controller\AlbumsController:details');
-$app->get('/search', 'App\Controller\AlbumsController:search');
-$app->any('/form', 'App\Controller\AlbumsController:form');
-$app->get('/api', 'App\Controller\ApiController:search');
+$app->get('/', 'App\Controller\AlbumsController::default');
+$app->get('/details/{id:[0-9]+}', 'App\Controller\AlbumsController::details');
+$app->get('/search', 'App\Controller\AlbumsController::search');
+$app->any('/form', 'App\Controller\AlbumsController::form');
+$app->get('/api', 'App\Controller\ApiController::search');
 
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
-$errorMiddleware->setErrorHandler(
-    Slim\Exception\HttpNotFoundException::class,
-    function (Psr\Http\Message\ServerRequestInterface $request) use ($container) {
-        $controller = new App\Controller\ExceptionController($container);
-        return $controller->notFound($request);
-    }
-);
 
 
 $app->run();
